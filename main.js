04de1136b597a7b73392853dd2ff7ab2b996add1
Billy Rhoades <billyarhoades@gmail.com>
2017-03-25 12:11:14 -0500

    Now using express' filesize limits.

--- main.js
+++ main.js
@@ -7,8 +7,9 @@ const filesizeParser = require('filesize-parser');
 
 const app = express();
 
-app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.json({limit: config.get("server.storage.size_limit")}));       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  limit: config.get("server.storage.size_limit"),
   extended: true
 }));
 
